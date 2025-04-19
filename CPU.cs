b959545bd5f1a947017c214fd33198bff2b1e378
workrom.24 <workrom24@gmail.com>
2025-04-19 14:56:16 +0300

    Individual task

---
+++ CPU.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab4
+{
+    class CPU
+    {
+        public string Model;
+        public string Manufacturer;
+        public int Cores;
+        public int Threads;
+        public double Frequency;
+        public int CacheMB;
+        public int TDPWatts;
+        public bool HasHyperThreading;
+
+        public double CalculatePerformance()
+        {
+            return Threads * Frequency;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,63 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        test_cpu();
+    }
+    private static void test_cpu()
+    {
+        Console.Write("Введiть модель процесора: ");
+        string model = Console.ReadLine();
+
+        Console.Write("Введiть виробника: ");
+        string manufacturer = Console.ReadLine();
+
+        Console.Write("Введiть кiлькiсть ядер: ");
+        int cores = int.Parse(Console.ReadLine());
+
+        Console.Write("Введiть кiлькiсть потокiв: ");
+        int threads = int.Parse(Console.ReadLine());
+
+        Console.Write("Введiть частоту (ГГц): ");
+        double frequency = double.Parse(Console.ReadLine());
+
+        Console.Write("Введiть розмiр кешу (МБ): ");
+        int cache = int.Parse(Console.ReadLine());
+
+        Console.Write("Введiть TDP (Вт): ");
+        int tdp = int.Parse(Console.ReadLine());
+
+        Console.Write("Чи пiдтримує Hyper-Threading? (y - так, n - нi): ");
+        ConsoleKeyInfo keyHyperThreading = Console.ReadKey(); Console.WriteLine();
+
+        CPU myCPU = new CPU();
+
+        myCPU.Model = model;
+        myCPU.Manufacturer = manufacturer;
+        myCPU.Cores = cores;
+        myCPU.Threads = threads;
+        myCPU.Frequency = frequency;
+        myCPU.CacheMB = cache;
+        myCPU.TDPWatts = tdp;
+        myCPU.HasHyperThreading = keyHyperThreading.Key == ConsoleKey.Y;
+
+        double performance = myCPU.CalculatePerformance();
+
+        Console.WriteLine();
+        Console.WriteLine("Інформація про процесор:");
+        Console.WriteLine($" Модель: {myCPU.Model}");
+        Console.WriteLine($" Виробник: {myCPU.Manufacturer}");
+        Console.WriteLine($" Кiлькiсть ядер: {myCPU.Cores}");
+        Console.WriteLine($" Кiлькiсть потокiв: {myCPU.Threads}");
+        Console.WriteLine($" Частота: {myCPU.Frequency} ГГц");
+        Console.WriteLine($" Кеш: {myCPU.CacheMB} МБ");
+        Console.WriteLine($" TDP: {myCPU.TDPWatts} Вт");
+        Console.WriteLine(myCPU.HasHyperThreading ? " Пiдтримує Hyper-Threading" : " Не пiдтримує Hyper-Threading");
+        Console.WriteLine();
+        Console.WriteLine($" Індекс продуктивностi: {performance:F2}");
+    }
+
+    private void test_town()
+    {
         Console.Write("Введiть назву мiста: "); string sName = Console.ReadLine();
         Console.Write("Введiть назву краiни: "); string sCountry = Console.ReadLine();
         Console.Write("Введiть назву регiону: "); string sRegion = Console.ReadLine();
@@ -20,11 +77,10 @@ internal class Program
         OurTown.HasPort = keyHasPort.Key == ConsoleKey.Y ? true : false;
         OurTown.HasAirport = keyHasAirport.Key == ConsoleKey.Y ? true : false;
 
-
         double YearIncomePerInhabitant = OurTown.GetYearIncomePerInhabitant();
         Console.WriteLine();
         Console.WriteLine(" "); Console.WriteLine(" Данi про об`ект: ");
-        Console.WriteLine(" "); Console.WriteLine(" Назва: " +OurTown.Name);
+        Console.WriteLine(" "); Console.WriteLine(" Назва: " + OurTown.Name);
         Console.WriteLine(" Країна: " + OurTown.Country);
         Console.WriteLine(" Регiон: " + OurTown.Region);
         Console.WriteLine(" Кiлькiсть населення: " +
@@ -32,8 +88,8 @@ internal class Program
         Console.WriteLine(" Рiчний дохiд: " +
         OurTown.YearIncome.ToString(" 0.00 "));
         Console.WriteLine(" Площа: " + OurTown.Square.ToString(" 0.000 "));
-        Console.WriteLine(OurTown.HasPort ? " У мiстi є порт" :" У мiстi нема порту");
-        Console.WriteLine(OurTown.HasAirport ? " У мiстi є аеропорт" :" У мiстi нема аеропорту");
+        Console.WriteLine(OurTown.HasPort ? " У мiстi є порт" : " У мiстi нема порту");
+        Console.WriteLine(OurTown.HasAirport ? " У мiстi є аеропорт" : " У мiстi нема аеропорту");
         Console.WriteLine();
         Console.WriteLine(" Середнiй річний дохід на одного громадянина:" + YearIncomePerInhabitant.ToString("0.00"));
     }
